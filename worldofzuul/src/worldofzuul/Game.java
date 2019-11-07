8818d821635b80043ea77f7061bd578d1a0219b3
Wahid Tobias <winberg19@outlook.dk>
2019-11-07 13:00:04 +0100

    Cleaned up the code

--- worldofzuul/src/worldofzuul/Game.java
+++ worldofzuul/src/worldofzuul/Game.java
@@ -23,20 +23,20 @@ public class Game {
 
     private void createRooms() {
         Garbage grb1, grb2, grb3, grb4, grb5, grb6, grb7, grb8, grb9, grb10, grb11, grb12;
-        grb1 = new Garbage("can", 2, 1);
-        grb2 = new Garbage("thermosMug", 1, 1);
-        grb3 = new Garbage("wineBottle", 3, 1);
-        grb4 = new Garbage("thermosFlask", 2, 1);
-        grb5 = new Garbage("mirror", 3, 1);
-        grb6 = new Garbage("perfumeContainer", 3, 1);
-        grb7 = new Garbage("cokeBottle", 1, 1);
-        grb8 = new Garbage("key", 2, 1);
-        grb9 = new Garbage("aquarium", 3, 1);
-        grb10 = new Garbage("lunchbox", 1, 1);
-        grb11 = new Garbage("babyBottle", 1, 1);
-        grb12 = new Garbage("handlebarBasket", 2, 1);
+        grb1 = new Garbage("Can", 2, 1);
+        grb2 = new Garbage("ThermosMug", 1, 1);
+        grb3 = new Garbage("WineBottle", 3, 1);
+        grb4 = new Garbage("ThermosFlask", 2, 1);
+        grb5 = new Garbage("Mirror", 3, 1);
+        grb6 = new Garbage("PerfumeContainer", 3, 1);
+        grb7 = new Garbage("CokeBottle", 1, 1);
+        grb8 = new Garbage("Key", 2, 1);
+        grb9 = new Garbage("Aquarium", 3, 1);
+        grb10 = new Garbage("Lunchbox", 1, 1);
+        grb11 = new Garbage("BabyBottle", 1, 1);
+        grb12 = new Garbage("HandlebarBasket", 2, 1);
 
-        Room outside = new Room("now in front of the staff room", 0, new File("Resources/Facts/BatteryFacts.csv"));
+        outside = new Room("now in front of the staff room", 0, new File("Resources/Facts/BatteryFacts.csv"));
         Room plasticCon = new Room("at the plastic container", 1, new File("Resources/Facts/PlasticFacts.csv"));
         Room metalCon = new Room("at the metal container", 2, new File("Resources/Facts/MetalFacts.csv"));
         Room glassCon = new Room("at the glass container", 3, new File("Resources/Facts/GlassFacts.csv"));
@@ -66,17 +66,23 @@ public class Game {
             Command command = parser.getCommand();
             finished = processCommand(command);
         }
-        System.out.println("Thank you for playing. Good bye.");
+        System.out.println("Thank you for playing RecycleHero!");
     }
-
-    private void printWelcome() {
-        createUsername();
-        
-        System.out.println("Welcome to RecycleHero, " + username + "!");
-        System.out.println("You're an employee at a recycling station and just arrived for work.");
-        System.out.println("Your job is to sort and collect the garbage that are littering on the ground.");
-        System.out.println("Write '" + CommandWord.HELP + "' if you need help.\n");
-        System.out.println(currentRoom.getLongDescription());
+    
+    private boolean quit(Command command) {
+        boolean hasEnded = false;
+        if (command.hasSecondWord()) {
+            System.out.println("Quit doesn't need a second command.");
+            return hasEnded;
+        } else {
+            if (currentRoom.equals(outside)) {
+                System.out.println("Your final number of points is: " + startPoint + "!");
+                hasEnded = true;
+            } else {
+                System.out.println("You have to be in front of the staff room.");
+            }
+            return hasEnded;
+        }
     }
 
     private boolean processCommand(Command command) {
@@ -85,7 +91,7 @@ public class Game {
         CommandWord commandWord = command.getCommandWord();
 
         if (commandWord == CommandWord.UNKNOWN) {
-            System.out.println("I don't know what you mean...");
+            System.out.println("The commandword doesn't exist.");
             return false;
         }
 
@@ -100,38 +106,13 @@ public class Game {
         } else if (commandWord == CommandWord.DROP) {
             dropGarbage(command);
         } else if (commandWord == CommandWord.INVENTORY) {
-            printInventory();
+            inventory.printInventory();
         } else if (commandWord == CommandWord.LOOK) {
             printContainer();
         } else if (commandWord == CommandWord.DONE) {
             System.out.println("You're done with the game. Your score is [" + startPoint + "].");
         }
         return wantToQuit;
-    }
-
-    private void printHelp() {
-        System.out.println("Your command words are:");
-        parser.showCommands();
-        System.out.println("\nGo: to move between rooms. Use direction after go command");
-        System.out.println("Quit: to leave the game.");
-        System.out.println("Take: to pick up items.");
-        System.out.println("Drop: to drop items.");
-        System.out.println("Inventory: to show your item(s) in your hands.");
-        System.out.println("Look: to look inside containers.");
-        System.out.println("Score: to see your score.");
-    }
-
-    public void printInventory() {
-        System.out.print("You're holding: ");
-        for (int i = 0; i < inventory.getInventory().size(); i++) {
-            System.out.print((i == 0 ? "" : ", ") + inventory.getInventory().get(i).getGarbageName());
-        }
-
-        if (inventory.getInventory().isEmpty()) {
-            System.out.print("nothing.\n");
-        } else {
-            System.out.print(".\n");
-        }
     }
 
     private void goRoom(Command command) {
@@ -144,7 +125,7 @@ public class Game {
         Room nextRoom = currentRoom.getExit(direction);
 
         if (nextRoom == null) {
-            System.out.println("There is no door!");
+            System.out.println("The direction doesn't exist.");
         } else {
             currentRoom = nextRoom;
             System.out.println(currentRoom.getLongDescription());
@@ -166,12 +147,9 @@ public class Game {
                     startPoint -= currentRoom.getContainer().get(i).getPoints();
                 }
                 inventory.getInventory().add(currentRoom.getContainer().get(i));
-                System.out.println(currentRoom.getContainer().get(i).getGarbageName() + " has been added");
+                System.out.println(currentRoom.getContainer().get(i).getGarbageName() + " has been added to the inventory.");
                 currentRoom.getContainer().remove(i);
-            } else {
-                System.out.println("The item was not in the container");
             }
-
         }
 
     }
@@ -195,12 +173,9 @@ public class Game {
                 }
 
                 currentRoom.getContainer().add(inventory.getInventory().get(i));
-                System.out.println(inventory.getInventory().get(i).getGarbageName() + " has been added to " + currentRoom.typeOfContainer() + " container");
+                System.out.println(inventory.getInventory().get(i).getGarbageName() + " has been added to " + currentRoom.typeOfContainer() + " container.");
                 inventory.getInventory().remove(i);
-            } else {
-                System.out.println("The item was not found in your inventory");
             }
-
         }
 
     }
@@ -224,26 +199,32 @@ public class Game {
             System.out.println("The " + currentRoom.typeOfContainer() + " container is empty.");
         }
     }
-
-    private boolean quit(Command command) {
-        boolean hasEnded = false;
-        if (command.hasSecondWord()) {
-            System.out.println("Quit doesn't need a second command.");
-            return hasEnded;
-        } else {
-            if (currentRoom.equals(outside)) {
-                System.out.println("Points: " + startPoint);
-                hasEnded = true;
-            } else {
-                System.out.println("You have to be in front of the staff room.");
-            }
-            return hasEnded;
-        }
+    
+    private void printWelcome() {
+        createUsername();
+        
+        System.out.println("Welcome to RecycleHero, " + username + "!");
+        System.out.println("You're an employee at a recycling station and just arrived for work.");
+        System.out.println("Your job is to sort and collect the garbage that are littering on the ground.");
+        System.out.println("Write '" + CommandWord.HELP + "' if you need help.\n");
+        System.out.println(currentRoom.getLongDescription());
+    }
+    
+    private void printHelp() {
+        System.out.println("Your command words are:");
+        parser.showCommands();
+        System.out.println("\nGo: move between rooms. (Use direction as second command word)");
+        System.out.println("Quit: leave the game.");
+        System.out.println("Take: pick up items.");
+        System.out.println("Drop: drop items.");
+        System.out.println("Inventory: show item(s) in your hands.");
+        System.out.println("Look: look inside containers.");
+        System.out.println("Score: see your current score.");
     }
 
     public String createUsername() {
         Scanner myScanner = new Scanner(System.in);
-        System.out.print("Type your name: ");
+        System.out.print("Type your username: ");
         username = myScanner.nextLine();
         return username;
     }
