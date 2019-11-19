dc2ccedae6f96d609617f3033e7a7b4f838f9890
Wahid Tobias <winberg19@outlook.dk>
2019-11-19 09:00:10 +0100

    Ant to maven

--- worldofzuul/worldofzuul2/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module worldofzuul.presentation {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens worldofzuul.presentation to javafx.fxml;
-    exports worldofzuul.presentation;
-}
--- worldofzuul/worldofzuul2/src/main/java/worldofzuul/domain/Main.java
+++
@@ -1,7 +0,0 @@
-package worldofzuul.domain;
-
-public class Main {
-    public static void main(String[] args) {
-        App.load(args);
-    }
-}
